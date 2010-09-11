078a0376f809626ab7da40198d24ef2619551dc4
Ryan Bigg <radarlistener@gmail.com>
2010-09-11 13:58:29 +1000

    Bump to beta5

--- lib/lookup.rb
+++ lib/lookup.rb
@@ -7,7 +7,7 @@ ENV['BUNDLE_GEMFILE'] = gemfile
 Bundler.require(:default)
 
 module Lookup
-  VERSION = "1.0.0.beta4"
+  VERSION = "1.0.0.beta5"
   APIS = []
   
   class APINotFound < StandardError; end
